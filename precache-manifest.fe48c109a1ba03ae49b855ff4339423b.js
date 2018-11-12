531916464deb7d94fbc97cbf9b2cdcc807c54d16
mhanley00 <mehanley2@gmail.com>
2018-11-12 13:12:55 -0500

    Updates

---
+++ precache-manifest.fe48c109a1ba03ae49b855ff4339423b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/itunesapi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5417af44425abc6b404",
+    "url": "/itunesapi/static/js/runtime~main.b5417af4.js"
+  },
+  {
+    "revision": "14612b45caa5bb7f934b",
+    "url": "/itunesapi/static/js/main.14612b45.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/itunesapi/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "14612b45caa5bb7f934b",
+    "url": "/itunesapi/static/css/main.14471d22.chunk.css"
+  },
+  {
+    "revision": "2804cbce839e8cab29099f26f4c2e4ae",
+    "url": "/itunesapi/index.html"
+  }
+];
