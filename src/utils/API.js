7646336f9ec610a331a501adc6654c92c6b3ffc6
mhanley00 <mehanley2@gmail.com>
2018-11-17 11:47:45 -0500

    fixed axios

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const BASEURL = "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term="; //https://itunes.apple.com/search?term=jack+johnson&entity=musicVideo
-// const ALBUM = "&entity=album"
+const ALBUM = "&entity=album"
 
 export default {
   search: function(query) {
-    return axios.get(BASEURL + query);
-    // return axios.get(BASEURL + query + ALBUM);
+    // return axios.get(BASEURL + query);
+    return axios.get(BASEURL + query + ALBUM);
   }
 };
